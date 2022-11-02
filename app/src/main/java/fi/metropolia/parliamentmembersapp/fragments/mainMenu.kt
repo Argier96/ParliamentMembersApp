ecdc98fe1d0e4b8fd8d78a879359200f16bb777f
riteshg <riteshg@metropolia.fi>
2022-11-02 23:46:50 +0200

    Added functionality to buttons on mainMenu fragment i.e. jump into another fragment when the buttons are pressed.

--- app/src/main/java/fi/metropolia/parliamentmembersapp/fragments/mainMenu.kt
+++ app/src/main/java/fi/metropolia/parliamentmembersapp/fragments/mainMenu.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.squareup.picasso.Picasso
 import fi.metropolia.parliamentmembersapp.R
 
@@ -20,8 +21,17 @@ class mainMenu : Fragment() {
     ): View? {
         binding = DataBindingUtil.inflate<FragmentMainMenuBinding>(inflater, R.layout.fragment_main_menu,container,false)
 
-        Picasso.get().load("https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Logo_of_the_Parliament_of_Finland.svg/2880px-Logo_of_the_Parliament_of_Finland.svg.png").into(binding.imageView)
+        //Using picasso to load image from the internet for parliament logo
+        val IMAGE_URL = "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Logo_of_the_Parliament_of_Finland.svg/2880px-Logo_of_the_Parliament_of_Finland.svg.png"
+        Picasso.get().load(IMAGE_URL).into(binding.imageView)
 
+        //Adding functionality for buttons
+        binding.btnByParties.setOnClickListener {
+            findNavController().navigate(R.id.action_mainMenu_to_partyList)
+        }
+        binding.btnAllMembers.setOnClickListener {
+            findNavController().navigate(R.id.action_mainMenu_to_membersList)
+        }
 
         return binding.root
     }
